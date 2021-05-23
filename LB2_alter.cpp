cd1754eef382938bb683d2423a14f028955c2e1d
Chandi Charan Mahato <55855799+Chandi977@users.noreply.github.com>
2021-05-23 15:48:25 +0530

    Add files via upload

---
+++ LB2_alter.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class Solution
+{
+public:
+    int kthSmallest(int arr[], int l, int r, int k)
+    {
+        r=r+1;
+        sort(arr,arr+r);
+        return arr[k-1];
+    }
+};
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        for(int i=0; i<number_of_elements; i++)
+            cin>>a[i];
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a,0,number_of_elements-1,k)<<endl;
+    }
+    return 0;
+}
